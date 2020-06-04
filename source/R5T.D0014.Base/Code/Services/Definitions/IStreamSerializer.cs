7e3e29356f476f6b5dcd4008b0bf260d09439a16
MinexAutomation <minex.automation@gmail.com>
2020-06-03 17:39:50 -0700

    Added R5T.D0014.Base, and IStreamSerializer<T> interface.

---
+++ source/R5T.D0014.Base/Code/Services/Definitions/IStreamSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0014
+{
+    /// <summary>
+    /// De/serializes an object of type <typeparamref name="T"/> to a <see cref="Stream"/>.
+    /// </summary>
+    public interface IStreamSerializer<T>
+    {
+        /// <summary>
+        /// Deserializes an object from a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="filePath">The rooted file path to use.</param>
+        Task<T> DeserializeAsync(string filePath);
+
+        /// <summary>
+        /// Serializes an object to a <see cref="Stream"/>.
+        /// </summary>
+        Task SerializeAsync(T value);
+    }
+}
